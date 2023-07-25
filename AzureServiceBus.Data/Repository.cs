b8092bad56d939deb0a69cb015ab9c4ea548db0c
Krzysztof Gwóźdź <krzysztof.gwozdz@outlook.com>
2023-07-25 14:49:43 +0200

    Get rid of obsolete package

--- AzureServiceBus.Data/Repository.cs
+++ AzureServiceBus.Data/Repository.cs
@@ -1,16 +1,13 @@
 using Azure.Messaging.ServiceBus.Administration;
-using Microsoft.Azure.ServiceBus.Management;
 
 namespace AzureServiceBus.Data;
 
 public class Repository
 {
-    protected readonly ManagementClient ManagementClient;
     protected readonly ServiceBusAdministrationClient ServiceBusAdministrationClient;
 
     protected Repository(string connectionString)
     {
-        ManagementClient = new ManagementClient(connectionString);
         ServiceBusAdministrationClient = new ServiceBusAdministrationClient(connectionString);
     }
 }
--- AzureServiceBus.Data/Subscription.cs
+++ AzureServiceBus.Data/Subscription.cs
@@ -1,14 +1,13 @@
 using Azure.Messaging.ServiceBus.Administration;
-using Microsoft.Azure.ServiceBus.Management;
 
 namespace AzureServiceBus.Data;
 
 public record Subscription(string? Name, string? ForwardTo, long? MessageCount, long? DeadLetterMessageCount)
 {
-    public static Subscription Create(SubscriptionRuntimeProperties subscriptionRuntimeProperties, SubscriptionDescription subscriptionDescription, string[]? fields) =>
+    public static Subscription Create(SubscriptionRuntimeProperties subscriptionRuntimeProperties, SubscriptionProperties subscriptionProperties, string[]? fields) =>
         new(
             fields is null || fields.Contains("subscription.name") ? subscriptionRuntimeProperties.SubscriptionName : null,
-            fields is null || fields.Contains("subscription.forwardTo") ? subscriptionDescription.ForwardTo[(subscriptionDescription.ForwardTo.IndexOf("net/", StringComparison.InvariantCultureIgnoreCase) + 4)..] : null,
+            fields is null || fields.Contains("subscription.forwardTo") ? subscriptionProperties.ForwardTo[(subscriptionProperties.ForwardTo.IndexOf("net/", StringComparison.InvariantCultureIgnoreCase) + 4)..] : null,
             fields is null || fields.Contains("subscription.messageCount") ? subscriptionRuntimeProperties.ActiveMessageCount : null,
             fields is null || fields.Contains("subscription.deadLetterMessageCount") ? subscriptionRuntimeProperties.DeadLetterMessageCount : null
         );
