61acc36720bf77ab4cce62e96cbe0e0c4268270d
Krzysztof Gwóźdź <krzysztof.gwozdz@outlook.com>
2023-07-25 15:46:32 +0200

    Fix getting queues without forwarding

--- AzureServiceBus.Data/Subscription.cs
+++ AzureServiceBus.Data/Subscription.cs
@@ -7,7 +7,7 @@ public record Subscription(string? Name, string? ForwardTo, long? MessageCount, 
     public static Subscription Create(SubscriptionRuntimeProperties subscriptionRuntimeProperties, SubscriptionProperties subscriptionProperties, string[]? fields) =>
         new(
             fields is null || fields.Contains("subscription.name") ? subscriptionRuntimeProperties.SubscriptionName : null,
-            fields is null || fields.Contains("subscription.forwardTo") ? subscriptionProperties.ForwardTo[(subscriptionProperties.ForwardTo.IndexOf("net/", StringComparison.InvariantCultureIgnoreCase) + 4)..] : null,
+            (fields is null || fields.Contains("subscription.forwardTo")) && subscriptionProperties.ForwardTo is not null ? subscriptionProperties.ForwardTo[(subscriptionProperties.ForwardTo.IndexOf("net/", StringComparison.InvariantCultureIgnoreCase) + 4)..] : null,
             fields is null || fields.Contains("subscription.messageCount") ? subscriptionRuntimeProperties.ActiveMessageCount : null,
             fields is null || fields.Contains("subscription.deadLetterMessageCount") ? subscriptionRuntimeProperties.DeadLetterMessageCount : null
         );
